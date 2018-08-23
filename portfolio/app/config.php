f53faeba73308388bfa66f29ba6385c603258c6e
katemostepan <35233028+mostepan@users.noreply.github.com>
2018-08-23 11:49:17 +0300

    Add files via upload

---
+++ portfolio/app/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * A Design by kate.mostepan
+Author: kate.mostepan
+Author URL: http://kate.mostepan.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+$current_page_uri = $_SERVER['REQUEST_URI'];
+$part_url = explode("/", $current_page_uri);
+$page_name = end($part_url);
+$email_id = "kate.mostepan@gmail.com";
+?>
